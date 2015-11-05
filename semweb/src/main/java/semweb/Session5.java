0907811238d95b3529857f9b5df3d781a46aa29d
zunzunwang <zunzunwang@gmail.com>
2015-11-05 12:13:35 +0100

    ajouter le nombre
    ajouter commentaires de nombre.
    new fonction for filter

--- semweb/src/main/java/semweb/Session5.java
+++ semweb/src/main/java/semweb/Session5.java
@@ -30,7 +30,6 @@ public class Session5 {
 		String xsd = "http://www.w3.org/2001/XMLSchema#";
 		*/
 		String v = "http://www.emse.fr/~zimmermann/Teaching/SemWeb/vocab.ttl#";
-//		String Scientist ="http://www.emse.fr/~zimmermann/Teaching/SemWeb/vocab.ttl#Scientist";
 		m.read(w3cstaff,"RDFA");
 		m.write(System.out);
 	/*	
@@ -40,17 +39,20 @@ public class Session5 {
 		m.setNsPrefix("xsd", xsd);
 		*/
 		m.setNsPrefix("v", v);	
-		Resource Scientist = m.createResource(v + "Scientist");
-		
+		Resource Scientist = m.createResource(v + "Scientist");		
 		// select all the resources with a VCARD.FN property
-		ResIterator iter = m.listSubjectsWithProperty(RDF.type);
+		ResIterator iter = m.listSubjectsWithProperty(RDF.type, Scientist);
 		if (iter.hasNext()) {
 			System.out.println("The model contains those rdf:type :");
+			int nb=0;
 			 while (iter.hasNext()) {
-				 //if(iter.nextResource().getPropertyResourceValue(RDF.type)==(Scientist))
+				 //nest resource第一回是对应于一行 下一次使用会跳到下一行每次使用都会跳行
+//			 if((iter.nextResource().getPropertyResourceValue(RDF.type)).equals(Scientist)){
 				 System.out.println("  " + iter.nextResource().getPropertyResourceValue(RDF.type));
-		
-			 }			
+				 nb++;
+//				 }
+			 }
+			 System.out.println("  nombre de scientist:"+nb);
 		}else {
             System.out.println("No rdf:type were found in the model");
         } 
